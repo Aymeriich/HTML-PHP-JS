3e16ffad3f9e28eff38e252b89f2b601e7352d83
Aymeriich <68127475+Aymeriich@users.noreply.github.com>
2021-03-31 10:46:19 +0200

    Add files via upload

---
+++ UF2_AC1/uytransfer/header.php
@@ -0,0 +1,7 @@
+<header class="navbar" style="font-family: Acme">
+    <h1 class="col-md col-12 text-md-left text-center">Uy!Transfer</h1>
+    <div class="col-md col-12 text-md-right">
+        <a href="index.php" class="col text-dark">Enviar archivo</a> | 
+        <a href="upload.php" class="col text-dark">Mis últimos archivos</a>
+    </div>
+</header>
