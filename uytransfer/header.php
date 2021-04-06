122b0f20a25c2e8cb6ba9c0bbda06cbeac394abb
Aymeriich <68127475+Aymeriich@users.noreply.github.com>
2021-04-06 20:04:31 +0200

    Delete uytransfer directory

--- uytransfer/header.php
+++
@@ -1,7 +0,0 @@
-<header class="navbar">
-    <h1 class="col-md col-12 text-md-left text-center" style="font-family: Acme">Uy!Transfer</h1>
-    <div class="col-md col-12 text-md-right">
-        <a href="index.php" class="col text-dark">Enviar archivo</a> | 
-        <a href="upload.php" class="col text-dark">Mis últimos archivos</a>
-    </div>
-</header>
